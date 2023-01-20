3b9f65387cff689a077c4b842bbf7a96538e4c8e
sml <smlrodrigues.dev@gmail.com>
2023-01-20 07:32:14 -0300

    Add Gameboard factory function
    and its tests

---
+++ src/scripts/__test__/gameboard.test.js
@@ -0,0 +1,76 @@
+import Gameboard from "../gameboard";
+import Ship from "../ship";
+
+test("Place ships", () => {
+  const game = Gameboard();
+  const x = 1;
+  const y = 1;
+  const ship = Ship(3);
+  game.placeShip(x, y, ship);
+  for (let i = y; i < y + 3; i++) {
+    expect(game.board[x][i].ship).toEqual(ship);
+  }
+});
+
+test("Place ships - cases not possible", () => {
+  const game = Gameboard();
+  expect(game.placeShip(1, 9, Ship(2))).toBe(false);
+  expect(game.placeShip(1, 9, Ship(1))).toBe(true);
+});
+
+test("Receive Attack - One attack", () => {
+  const game = Gameboard();
+  const ship = Ship(3);
+  game.placeShip(1, 1, ship);
+  game.receiveAttack(1, 1);
+
+  expect(game.board[1][1].ship.getTimesHit()).toBe(1);
+
+  game.receiveAttack(1, 2);
+  expect(game.board[1][2].ship.getTimesHit()).toBe(2);
+});
+
+test("Receive Attack - Repeat same Attack", () => {
+  const game = Gameboard();
+  const ship = Ship(3);
+  game.placeShip(1, 1, ship);
+  game.receiveAttack(1, 1);
+  game.receiveAttack(1, 1);
+  expect(game.board[1][1].ship.getTimesHit()).toBe(1);
+});
+
+test("Receive Attack - Sunk ship ", () => {
+  const game = Gameboard();
+  const ship = Ship(3);
+  game.placeShip(1, 1, ship);
+  game.receiveAttack(1, 1);
+  game.receiveAttack(1, 2);
+  game.receiveAttack(1, 3);
+  expect(game.board[1][1].ship.isSunk()).toBe(true);
+});
+
+test("Receive Attack - Missed attack", () => {
+  const game = Gameboard();
+  expect(game.board[1][1].attacked).toBe(false);
+  game.receiveAttack(1, 1);
+  expect(game.board[1][1].attacked).toBe(true);
+});
+
+test("Receive Attack - All ship been sunk", () => {
+  const game = Gameboard();
+
+  game.placeShip(1, 1, Ship(1));
+  game.placeShip(1, 2, Ship(1));
+  game.placeShip(1, 3, Ship(1));
+
+  expect(game.allSunk()).toBe(false);
+
+  game.receiveAttack(1, 1);
+  expect(game.allSunk()).toBe(false);
+
+  game.receiveAttack(1, 2);
+  expect(game.allSunk()).toBe(false);
+
+  game.receiveAttack(1, 3);
+  expect(game.allSunk()).toBe(true);
+});
---
+++ src/scripts/gameboard.js
@@ -0,0 +1,55 @@
+export default function Gameboard() {
+
+  const _createBoard = (size) => {
+    const board = [];
+    for (let i = 0; i < size; i++) {
+      board.push([]);
+      for (let j = 0; j < size; j++) {
+        board[i].push(
+          {
+            attacked: false,
+            ship: null
+          }
+        );
+      }
+    }
+    return board;
+  }
+
+  const board = _createBoard(10);
+
+  const _ships = [];
+
+  const placeShip = (x, y, ship) => {
+    if (x + ship.getLength() > 9 || y + ship.getLength() > 9 && ship.getLength() > 1) return false;
+
+    _ships.push(ship);
+
+    for (let i = y; i < ship.getLength() + y; i++) {
+      board[x][i].ship = ship;
+    } 
+
+    return true;
+  }
+
+  const receiveAttack = (x, y) => {
+
+    if (!board[x][y].attacked) {
+      board[x][y].attacked = true;
+
+      if (board[x][y].ship) {
+          board[x][y].ship.hit();
+      } else {
+        if(board[x][y] === null) {
+          board[x][y] = false
+        } 
+      }
+    }
+  }
+
+  const allSunk = () => {
+    return _ships.every((ship) => ship.isSunk() == true);
+  }
+
+  return { placeShip, board, receiveAttack, allSunk }
+};
